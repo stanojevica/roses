adbece2837ee05827570cc96fbdbd2442c6b5b19
stanojevica <52257766+stanojevica@users.noreply.github.com>
2019-12-05 18:46:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -330,7 +330,7 @@ function provera(){
 //Ubacivanje sadrzaja u div oAutoru
 
 var mojaSlika = document.createElement("img")
-mojaSlika.setAttribute('src','img/me1.jpg');
+mojaSlika.setAttribute('src','img/me1.JPG');
 mojaSlika.setAttribute('alt','autorSlika');
 document.getElementById("slikaAutor").appendChild(mojaSlika);
 
